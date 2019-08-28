c46a17ba61a3f86faedfb9f30dd057af32ec3516
JuliaNeumann <jn.julianeumann@gmail.com>
2019-08-28 21:05:24 +0200

    item router solved

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,25 @@ import { Router } from 'express'
 
 const router = Router()
 
+router
+  .route('/')
+  .get((req, res) => {
+    res.send({ message: 'get item' })
+  })
+  .post((req, res) => {
+    res.send({ message: 'post item' })
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    res.send({ message: 'get item by id' })
+  })
+  .put((req, res) => {
+    res.send({ message: 'put item' })
+  })
+  .delete((req, res) => {
+    res.send({ message: 'delete item' })
+  })
+
 export default router
