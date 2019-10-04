3ef10a6499b1c60b81888392ffcce59949e226a9
JuliaNeumann <jn.julianeumann@gmail.com>
2019-10-04 21:30:44 +0200

    finish controller exercise

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,23 +1,69 @@
 export const getOne = model => async (req, res) => {
-  return model.findOne({_id: req.params.id, createdBy: req.user._id})
-    .then((doc) => {
+  return model
+    .findOne({ _id: req.params.id, createdBy: req.user._id })
+    .then(doc => {
       if (doc) {
-        res.status(200);
-        res.json({data: doc});
+        res.status(200)
+        res.json({ data: doc })
       } else {
-        res.status(400);
-        res.end();
+        res.status(400)
+        res.end()
       }
-    });
+    })
 }
 
-export const getMany = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  return model.find({ createdBy: req.user._id }).then(docs => {
+    if (docs) {
+      res.status(200)
+      res.json({ data: docs })
+    } else {
+      res.status(400)
+      res.end()
+    }
+  })
+}
 
-export const createOne = model => async (req, res) => {}
+export const createOne = model => async (req, res) => {
+  return model.create({ createdBy: req.user._id, ...req.body }).then(doc => {
+    if (doc) {
+      res.status(201)
+      res.json({ data: doc })
+    }
+  })
+}
 
-export const updateOne = model => async (req, res) => {}
+export const updateOne = model => async (req, res) => {
+  return model
+    .findOneAndUpdate(
+      { _id: req.params.id, createdBy: req.user._id },
+      req.body,
+      { new: true }
+    )
+    .then(doc => {
+      if (doc) {
+        res.status(200)
+        res.json({ data: doc })
+      } else {
+        res.status(400)
+        res.end()
+      }
+    })
+}
 
-export const removeOne = model => async (req, res) => {}
+export const removeOne = model => async (req, res) => {
+  return model
+    .findOneAndRemove({ _id: req.params.id, createdBy: req.user._id }, req.body)
+    .then(doc => {
+      if (doc) {
+        res.status(200)
+        res.json({ data: doc })
+      } else {
+        res.status(400)
+        res.end()
+      }
+    })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
